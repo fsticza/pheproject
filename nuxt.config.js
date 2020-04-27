64d3fc935b736aeede8e327542c15e86f9f854d4
fsticza <ferenc.sticza@gmail.com>
2020-04-27 19:14:23 +0200

    fix: viewport meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,10 @@ module.exports = {
     title: 'PHE project development & consulting',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
+      },
       {
         hid: 'description',
         name: 'description',
